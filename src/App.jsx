97a11c18e9777211c7c8d4b0151ba9484bffebf3
lkai1 <kai-le@outlook.com>
2025-08-01 22:36:11 +0300

    remove carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styles from "./App.module.css"
 import About from "./About"
 import Experience from "./Experience"
-import ProjectsCarousel from "./ProjectsCarousel"
 import ProjectsList from "./ProjectsList"
 import Footer from "./Footer"
 
@@ -11,7 +10,6 @@ const App = () => {
     <div className={styles.main}>
       <About />
       <Experience />
-      <ProjectsCarousel />
       <ProjectsList />
       <Footer />
     </div>
