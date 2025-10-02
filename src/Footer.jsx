be56cd5495fe389d5fa21dcd92d094babdd775c3
lkai1 <kai-le@outlook.com>
2025-10-02 19:31:31 +0300

    update phone number

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             </div>
             <div className={styles.infoContainer}>
                 <img src="/phone.png" className={styles.infoTextIcon} />
-                <p className={styles.infoText}>+358449559127</p>
+                <p className={styles.infoText}>Lost sim card on vacation. Update soon.</p>
             </div>
             <div className={styles.infoContainer}>
                 <img src="/email.png" className={styles.infoTextIcon} />
