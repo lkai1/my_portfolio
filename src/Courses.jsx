52f3c8f46bd8fa05daebcbc102d05b0cbafcfde0
lkai1 <kai-le@outlook.com>
2026-01-04 21:12:27 +0200

    add aws certificate

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -14,6 +14,10 @@ const Courses = () => {
                     <p className={styles.courseName}>AWS Certified Cloud Practitioner: Exam Prep Plan</p>
                     <img src="/aws_exam_prep_plan_certified_cloud_practitioner_certification.png" className={styles.courseImage} />
                 </div>
+                <div className={styles.courseContainer}>
+                    <p className={styles.courseName}>AWS Developer Associate Certification Prep</p>
+                    <img src="/aws_developer_associate.png" className={styles.courseImage} />
+                </div>
             </div>
         </div>
     )
